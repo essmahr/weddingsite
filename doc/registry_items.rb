31130111adaf40a90096d20a05e08382eefde9f2
loopduplicate <scott@cabinroom.ca>
2014-01-19 10:12:34 -0800

    registry work

--- doc/registry_items.rb
+++ doc/registry_items.rb
@@ -1,28 +1,101 @@
 $registries = [
   {
     :id => 1,
-    :description => 'This is the first item',
-    :url => 'http://google.com'    
+    :title => "Wool Pendleton Blanket",
+    :url => "http://www.madeinoregon.com/Pendleton_Blanket_Crater_Lake_Full.html"    
   },
   {
 
     :id => 2,
-    :description => 'This is the second item',
-    :url => 'http://google.com'    
+    :title => "Pizza Stone",
+    :url => "http://google.com"    
   },
   {
     :id => 3,
-    :description => 'This is the third item',
-    :url => 'http://google.com'    
+    :title => "Ice Cream Maker",
+    :url => "http://www.cuisinart.ca/cuisinart_product.php?item_id=143&"    
   },
   {
     :id => 4,
-    :description => 'This is the fourth item',
-    :url => 'http://google.com'    
+    :title => "St. Germain Liqeur",
+    :note => "available at your local government liquor store."   
   },
   {
     :id => 5,
-    :description => 'This is the fifth item',
-    :url => 'http://google.com'    
+    :title => "Plenty Cookbook",
+    :note => "Available at your local independent bookstore."   
+  },
+  {
+    :id => 6,
+    :title => "Pillows",
+    :note => "Available at <a href='http://thebay.com' target='_blank'>The Bay</a> or your favourite home and bedding store."   
+  },
+  {
+    :id => 7,
+    :title => "Wooden Salad Bowl",
+  },
+  {
+    :id => 8,
+    :title => "Ceramic Oil Cruet",
+  },
+  {
+    :id => 9,
+    :title => "Turkish Throw Pillows",
+    :url => "http://www.etsy.com/ca/shop/PillowTalkOnEtsy",
+    :note => "We like <a href='https://www.etsy.com/listing/159298826/rustic-kilim-pillow-cover-ethnic-toss' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/175974994/bohemian-decor-ethnic-throw-pillow' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/173886080/rustic-decor-hand-embroidered-cushion' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/175884489/embroidered-throw-pillow-turkish-kilim' target='_blank'>This one</a>, and <a href='https://www.etsy.com/listing/174751882/modern-bohemian-decor-turkish-kilim' target='_blank'>This one</a>."   
+  },
+  {
+    :id => 10,
+    :title => "Subscription to Lucky Peach Magazine",
+    :description => "Nice Cookbook",
+    :url => "http://store.lky.ph/",
+  },
+  {
+    :id => 11,
+    :title => "Toaster Oven",
+    :url => "http://www.londondrugs.com/Cuisinart-Deluxe-Convection-Toaster-Oven---Brushed-Stainless-Steel---TOB-135C/L7726151,default,pd.html",
+    :note => "Available London Drugs or your favourite kitchen Appliance Store."   
+  },
+  {
+    :id => 12,
+    :title => "Pyrex 9x13 Baking Dish",
+    :description => "Nice Cookbook",
+    :url => false,
+    :note => "Available at your favourite cooking store."   
+  },
+  {
+    :id => 13,
+    :title => "Heath Rim Stacking Mugs",
+    :url => "https://www.etsy.com/listing/174173216/heath-rim-stacking-mugs-set-of-four",
+  },
+  {
+    :id => 14,
+    :title => "The Science of Good Cooking",
+    :note => "Avaialble at Munros in Victoria, your favourite local independent bookstore elsewhere, or <a href='http://www.amazon.ca/The-Science-Of-Good-Cooking/dp/1933615982' target='_blank'></a>."   
+  },
+  {
+    :id => 15,
+    :title => "New York Times&rsquo; 36 Hours &mdash; 150 Weekends in the USA and Canada",
+    :note => "Available at your local independent bookstore and <a href='http://www.taschen.com/pages/en/catalogue/lifestyle/all/06775/facts.the_new_york_times_36_hours_150_weekends_in_the_usa_canada.htm' target='_blank'>online</a>"   
+  },
+  {
+    :id => 16,
+    :title => "Sydney Hale Candle Company candles",
+    :url => "http://www.sydneyhaleco.com/products?product_category_id=10",
+  },
+  {
+    :id => 17,
+    :title => "White Dinnerware",
+    :note => "Available at The Bay."   
+  },
+  {
+    :id => 18,
+    :title => "Flatware",
+    :note => "Available at The Bay."   
+  },
+  {
+    :id => 19,
+    :title => "Set of water glasses",
+    :note => "Available at The Bay."   
   }
 ]
