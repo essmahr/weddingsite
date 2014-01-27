a8d5758489a9cac4771d9ff789f70fe47d538996
loopduplicate <scott@cabinroom.ca>
2014-01-26 18:20:05 -0800

    update registry items

--- doc/registry_items.rb
+++ doc/registry_items.rb
@@ -20,7 +20,7 @@ $registries = [
   },
   {
     :id => 4,
-    :title => "St. Germain Liqeur",
+    :title => "St. Germain Liqueur",
     :img => 'stgermain',
     :note => "available at your local government liquor store."   
   },
@@ -54,98 +54,80 @@ $registries = [
     :note => "We like <a href='https://www.etsy.com/listing/159298826/rustic-kilim-pillow-cover-ethnic-toss' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/175974994/bohemian-decor-ethnic-throw-pillow' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/173886080/rustic-decor-hand-embroidered-cushion' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/175884489/embroidered-throw-pillow-turkish-kilim' target='_blank'>This one</a>, and <a href='https://www.etsy.com/listing/174751882/modern-bohemian-decor-turkish-kilim' target='_blank'>This one</a>."   
   },
   {
-    :id => 9,
+    :id => 10,
     :title => "Turkish Throw Pillow",
     :url => "https://www.etsy.com/listing/175974994/bohemian-decor-ethnic-throw-pillow",
     :img => 'pillow2',
     :note => "We like <a href='https://www.etsy.com/listing/173886080/rustic-decor-hand-embroidered-cushion' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/175884489/embroidered-throw-pillow-turkish-kilim' target='_blank'>This one</a>, and <a href='https://www.etsy.com/listing/174751882/modern-bohemian-decor-turkish-kilim' target='_blank'>This one</a>."   
   },
   {
-    :id => 9,
+    :id => 11,
     :title => "Turkish Throw Pillow",
     :img => 'pillow3',
     :url => "http://www.etsy.com/listing/173886080/rustic-decor-hand-embroidered-cushion",
     :note => "We like <a href='https://www.etsy.com/listing/173886080/rustic-decor-hand-embroidered-cushion' target='_blank'>This one</a>, <a href='https://www.etsy.com/listing/175884489/embroidered-throw-pillow-turkish-kilim' target='_blank'>This one</a>, and <a href='https://www.etsy.com/listing/174751882/modern-bohemian-decor-turkish-kilim' target='_blank'>This one</a>."   
   },
   {
-    :id => 9,
+    :id => 12,
     :title => "Turkish Throw Pillow",
     :img => 'pillow4',
     :url => "http://www.etsy.com/listing/175884489/embroidered-throw-pillow-turkish-kilim"
   },
   {
-    :id => 9,
+    :id => 13,
     :title => "Turkish Throw Pillow",
     :img => 'pillow5',
     :url => "http://www.etsy.com/listing/174751882/modern-bohemian-decor-turkish-kilim"
   },
 
   {
-    :id => 10,
+    :id => 14,
     :title => "Subscription to Lucky Peach Magazine",
     :img => 'luckypeach',
     :description => "Nice Cookbook",
     :url => "http://store.lky.ph/"
   },
   {
-    :id => 11,
+    :id => 15,
     :title => "Toaster Oven",
     :img => 'toaster',
     :url => "http://www.londondrugs.com/Cuisinart-Deluxe-Convection-Toaster-Oven---Brushed-Stainless-Steel---TOB-135C/L7726151,default,pd.html",
     :note => "Available London Drugs or your favourite kitchen Appliance Store."   
   },
   {
-    :id => 12,
+    :id => 16,
     :title => "Pyrex 9x13 Baking Dish",
     :description => "Nice Cookbook",
     :img => 'pyrex',
     :url => false
   },
   {
-    :id => 13,
-    :title => "Heath Rim Stacking Mugs",
-    :img => 'mugs',
-    :url => "https://www.etsy.com/listing/174173216/heath-rim-stacking-mugs-set-of-four"
+    :id => 17,
+    :title => "Bread Knife",
+    :img => 'breadknife',
+    :url => false
   },
   {
-    :id => 14,
+    :id => 18,
     :title => "The Science of Good Cooking",
     :img => 'science',
     :note => "Avaialble at Munros in Victoria, your favourite local independent bookstore elsewhere, or <a href='http://www.amazon.ca/The-Science-Of-Good-Cooking/dp/1933615982' target='_blank'></a>."   
   },
   {
-    :id => 15,
+    :id => 19,
     :img => 'nyt',
     :title => "New York Times&rsquo; 36 Hours &mdash; 150 Weekends in the USA and Canada",
     :note => "Available at your local independent bookstore and <a href='http://www.taschen.com/pages/en/catalogue/lifestyle/all/06775/facts.the_new_york_times_36_hours_150_weekends_in_the_usa_canada.htm' target='_blank'>online</a>"   
   },
   {
-    :id => 16,
+    :id => 20,
     :title => "Sydney Hale Candle Company candles",
     :img => 'candle',
     :url => "http://www.sydneyhaleco.com/products?product_category_id=10"
   },
   {
-    :id => 17,
-    :title => "White Dinnerware",
-    :note => "Available at The Bay.",
-    :img => 'blanket' 
-  },
-  {
-    :id => 18,
-    :title => "Flatware",
-    :note => "Available at The Bay.",   
-    :img => 'blanket' 
-  },
-  {
-    :id => 19,
-    :title => "Set of water glasses",
-    :note => "Available at The Bay.",
-    :img => 'blanket' 
-  },
-  {
-    :id => 20,
+    :id => 21,
     :img => 'whetstone',
-    :title => "Knife Sharpening Stone"
+    :title => "Shun Knife Sharpening Stone"
   }
 ]
