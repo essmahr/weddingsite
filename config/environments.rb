3a74f5f9e4f80daecce9ad587b540ba6199ed593
loopduplicate <scott@cabinroom.ca>
2014-02-10 18:02:01 -0800

    postgres to sqlite

--- config/environments.rb
+++ config/environments.rb
@@ -4,14 +4,19 @@ configure :development do
 end
 
 configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
+ set :database, 'sqlite:///dev.db'
+ set :show_exceptions, false
 end
+
+#configure :production do
+# db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+#
+# ActiveRecord::Base.establish_connection(
+#   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#   :host     => db.host,
+#   :username => db.user,
+#   :password => db.password,
+#   :database => db.path[1..-1],
+#   :encoding => 'utf8'
+# )
+#end
