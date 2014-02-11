f4199d8243c54d902e170ac090ac7a0164ead153
loopduplicate <scott@cabinroom.ca>
2014-02-10 18:07:18 -0800

    back to postgres

--- config/environments.rb
+++ config/environments.rb
@@ -4,19 +4,14 @@ configure :development do
 end
 
 configure :production do
- set :database, 'sqlite:///dev.db'
- set :show_exceptions, false
-end
+ db = URI.parse(ENV['HEROKU_POSTGRESQL_CHARCOAL_URL'] || 'postgres:///localhost/mydb')
 
-#configure :production do
-# db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-#
-# ActiveRecord::Base.establish_connection(
-#   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#   :host     => db.host,
-#   :username => db.user,
-#   :password => db.password,
-#   :database => db.path[1..-1],
-#   :encoding => 'utf8'
-# )
-#end
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
